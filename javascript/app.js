bec05ba0008628c93a149fae91a580b178d8f6eb
Hernavch <hernandezvc1292@gmail.com>
2019-01-27 10:44:59 -0800

    Ajax url

--- javascript/app.js
+++ javascript/app.js
@@ -1,47 +1,55 @@
 var topics =["Alphabet", "Numbers", "Sesame Street"];
+var topicCount = 0;
 
-$("button").on("click", function(){
-var gifname = $(this).attr("name");
-console.log(gifname);
+
+// $(document.body).on("click", ".checkbox", function() 
+$(document.body).on("click", ".gif", function(){
+    var gifname = $(this).text();
+    // alert("here i am")
+    console.log(gifname);
 // var name= topics[i].name;
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=2C6OXg6eGjPol03IE8JAuBTJD5Yh9Oha&q=" + gifname + "&limit=10&offset=0&rating=PG&lang=en";
+     queryURL = "https://api.giphy.com/v1/gifs/search?api_key=2C6OXg6eGjPol03IE8JAuBTJD5Yh9Oha&q=" + gifname + "&limit=10&offset=0lang=en";
 
-$.ajax({
-    url: queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-})
+        $.ajax({
+            url: queryURL,
+            method:"GET"
+        }).then(function(response){
+            // console.log(response.data[0].analytics.onclick.url);
+            // var gify= response.data[g];
+             for(var g = 0; g < response.data; g ++){
+                 alert("come on!")
+               var gif_url= response.data[g].analytics.onclick.url;
+            //    console.log(response);
+               console.log(gif_url); 
+             }
+           
+        })
 
-});
+        });
 
-for(var i = 0; i < topics.length; i++) {
-    var btn= $("<button>");
-    var name= topics[i]
-    btn.addClass("giftogif ");
-    btn.text(name);
-    $(".subwrapper").append(btn);
-    btn.attr("giftogif", topics);
-};
+
 
 function makeABtn(){
-    // $(".subwrapper").empty();
+    $(".subwrapper").empty();
+
     for(var i = 0; i < topics.length; i++) {
+        topicCount++;
         var btn= $("<button>");
-        btn.addClass("giftogif");
-        btn.text(topics[i].name);
+        var name= topics[i]
+        btn.attr("class", "gif "+ topics[i]);
+        btn.attr("id", "topic-"+ topicCount);
+        btn.text(name);
         $(".subwrapper").append(btn);
-        btn.attr("data-name", topics);
-        // Portion keeps repeating btns
-// 
+        btn.attr("giftogif", topics);
+    };
+    
 
     }
 
-}
 var topic = $("#topic-input").val();
 
 $("#add-btn").on("click", function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var topic = $("#topic-input").val();
     topics.push(topic);
     console.log(topics);
@@ -49,4 +57,5 @@ $("#add-btn").on("click", function(event) {
     
 });
 
-console.log(this);
+makeABtn();
+console.log(topics);
